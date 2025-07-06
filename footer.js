6161f080bd6786ffa067d5c80963d124d561b17f
Vikram Anantha <markivanantha@the-Markiv-Computer-v5.local>
2025-07-06 13:48:45 -0400

    mid summer update

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ function loadFooter() {
     const footerHTML = `
         <footer>
             <div class="footer-content">
-                <p>Last updated: May 1 2025</p>
+                <p>Last updated: Jul 2025</p>
                 <p>Vikram Anantha</p>
                 <p><a href="https://github.com/vikramanantha/vikramanantha.github.io" target="_blank">View Source Code</a></p>
             </div>
